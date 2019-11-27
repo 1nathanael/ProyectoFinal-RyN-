1c854cffd968a12a5c656435850a3f482656dce3
raynier lara <raynier98@hotmail.es>
2019-11-26 23:31:46 -0400

    creando el controlador de permiso en base al modelo

---
+++ ProyectoFinal--RyN--/ProyectoFinal--RyN--/Controllers/PermisoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProyectoFinal__RyN__.Models;
+
+namespace ProyectoFinal__RyN__.Controllers
+{
+    public class PermisoController : Controller
+    {
+        private RecursosHumanosEntities db = new RecursosHumanosEntities();
+
+        // GET: Permiso
+        public ActionResult Index()
+        {
+            return View(db.Permiso.ToList());
+        }
+
+        // GET: Permiso/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permiso permiso = db.Permiso.Find(id);
+            if (permiso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permiso);
+        }
+
+        // GET: Permiso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Permiso/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id_Permiso,Id_Empleado,Inicio_Permiso,Fin_Permiso,Comentario")] Permiso permiso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Permiso.Add(permiso);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(permiso);
+        }
+
+        // GET: Permiso/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permiso permiso = db.Permiso.Find(id);
+            if (permiso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permiso);
+        }
+
+        // POST: Permiso/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id_Permiso,Id_Empleado,Inicio_Permiso,Fin_Permiso,Comentario")] Permiso permiso)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(permiso).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(permiso);
+        }
+
+        // GET: Permiso/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Permiso permiso = db.Permiso.Find(id);
+            if (permiso == null)
+            {
+                return HttpNotFound();
+            }
+            return View(permiso);
+        }
+
+        // POST: Permiso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Permiso permiso = db.Permiso.Find(id);
+            db.Permiso.Remove(permiso);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
